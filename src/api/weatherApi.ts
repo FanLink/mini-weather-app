0816f6d7c076bd6e638ab06de23a7a2dbbb5899a
Phan Linh <pllinh92@gmail.com>
2021-08-13 10:34:27 +0700

    updated readme and some comments

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -2,10 +2,17 @@ import { Location, WeatherDetails } from 'models';
 import axiosClient from './axiosClient';
 
 const weatherApi = {
+  // # @name locationSearch
+  // params: query: string
+  // PATCH {{baseUrl}}/search/?query=${query}
   locationSearch(query: string): Promise<Location[]> {
     const url = `/search/?query=${query}`;
     return axiosClient.get(url);
   },
+
+  // # @name getWeatherByLocationId
+  // params: woeid: number
+  // PATCH {{baseUrl}}/${woeid}
   getWeatherByLocationId(woeid: number): Promise<WeatherDetails> {
     const url = `/${woeid}`;
     return axiosClient.get(url);
